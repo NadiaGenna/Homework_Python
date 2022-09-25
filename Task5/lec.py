d5be12d7301d88daf2a879514c9f730ae17ea6e5
«Nadezhda <nadezhdagenna@mail.ru>
2022-09-26 00:32:34 +0300

    Task5 is finished!

--- Task5/lec.py
+++ Task5/lec.py
@@ -3,10 +3,10 @@
 # A (3,6); B (2,1) -> 5,09
 # A (7,-5); B (1,-1) -> 7,21
 
-print('Enter coordinates of point А:')
+print("Enter coordinates of point А:")
 x_A = float(input('X: '))
 y_A = float(input('Y: '))
-print("Enter coordinates point B:")
+print("Enter coordinates of point B:")
 x_B = float(input('X: '))
 y_B = float(input('Y: '))
 from math import sqrt
